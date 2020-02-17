efde9bd466392b29254cc9de29847f616ac71f0a
screwyterror83 <screwyterror83@gmail.com>
2020-02-16 17:48:02 -0800

    Enable Django admin for user profile model

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -2,28 +2,29 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser
 from django.contrib.auth.models import PermissionsMixin
 from django.contrib.auth.models import BaseUserManager
-# from unittest.util import _MAX_LENGTH
-# from virtualenv import REQUIRED_FILES
+# from unittest.util import MAX_LENGTH
+# from virtualenv import REQUIRED_FILEDS
 
 class UserProfileManager(BaseUserManager):
     """Manager for user profile"""
     
-    def create_user(self,email,name,password=None):
+    def create_user(self, email, name, password=None):
         """Create a new user profile"""
         if not email:
             raise ValueError('User must have an email address')
         
         email = self.normalize_email(email)
         user = self.model(email=email, name=name)
-        
+
+    
         user.set_password(password)
         user.save(using=self._db)
         
         return user
     
-    def create_superuser(self,email,name,password):
+    def create_superuser(self, email, name, password):
         """"Create and save a new superuser with given details"""
-        user = self.create_user(email,name,password)
+        user = self.create_user(email, name, password)
         
         user.is_superuser = True
         user.is_staff = True
@@ -41,7 +42,7 @@ class UserProfile(AbstractBaseUser,PermissionsMixin):
     objects = UserProfileManager()
     
     USERNAME_FIELD = 'email'
-    REQUIRED_FILES = ['name']
+    REQUIRED_FIELDS = ['name']
     
     def get_full_name(self):
         """Retrieve full name of user"""
